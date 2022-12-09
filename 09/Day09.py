0601f71ff4fc4c0188339279d9a4bac6ef5d7891
furmarie <rahuljan@gmail.com>
2022-12-10 00:03:52 +0530

    optimized day 9 solution

--- 09/Day09.py
+++ 09/Day09.py
@@ -2,7 +2,7 @@ def solve(num_tails):
     with open("input.txt", "r") as f:
         ans = set()
         num_tails = num_tails
-        tails_pos = [(0, 0) for _ in range(num_tails + 1)]
+        tails_pos = [(10000, 10000) for _ in range(num_tails + 1)]
 
         def dist(a, b):
             return (pow(b[0] - a[0], 2) + pow(b[1] - a[1], 2))
@@ -11,41 +11,42 @@ def solve(num_tails):
             direction, n_moves = l.rstrip().split()            
 
             for _ in range(int(n_moves)):
-                h_pos = tails_pos[0]
+                (h_x, h_y) = tails_pos[0]
 
                 match direction:
                     case "U":
-                        h_pos = (h_pos[0], h_pos[1] + 1)
+                        h_y += 1
 
                     case "D":
-                        h_pos = (h_pos[0], h_pos[1] - 1)
+                        h_y -= 1
 
                     case "L":
-                        h_pos = (h_pos[0] - 1, h_pos[1])
+                        h_x -= 1
 
                     case "R":
-                        h_pos = (h_pos[0] + 1, h_pos[1])
+                        h_x += 1
 
-                tails_pos[0] = h_pos
+                tails_pos[0] = (h_x, h_y)
                 
                 for i in range(1, num_tails + 1):
-                    if dist(tails_pos[i], tails_pos[i - 1]) >= 3: 
-                        if tails_pos[i][0] != tails_pos[i - 1][0] and tails_pos[i][1] != tails_pos[i - 1][1]:
-                            dx = [1, 1, -1, -1]
-                            dy = [1, -1, 1, -1]
-                        else:
-                            dx = [0, 1, 0, -1]
-                            dy = [1, 0, -1, 0]
+                    x_sign = -1 if (tails_pos[i - 1][0] - tails_pos[i][0]) < 0 else 1
+                    x_offset = abs(tails_pos[i - 1][0] - tails_pos[i][0])
 
-                        best = tails_pos[i]
-                        for k in range(4):
-                            nx = tails_pos[i][0] + dx[k]
-                            ny = tails_pos[i][1] + dy[k]
+                    y_sign = -1 if (tails_pos[i - 1][1] - tails_pos[i][1]) < 0 else 1
+                    y_offset = abs(tails_pos[i - 1][1] - tails_pos[i][1])
 
-                            if(dist((nx, ny), tails_pos[i - 1]) < dist(tails_pos[i - 1], best)):
-                                best = (nx, ny)
+                    dx, dy = 0, 0
+                    
+                    if x_offset > 1:
+                        dx += 1
+                        if y_offset > 0:
+                            dy += 1
+                    elif y_offset > 1:
+                        dy += 1
+                        if x_offset > 0:
+                            dx += 1
 
-                        tails_pos[i] = best
+                    tails_pos[i] = (tails_pos[i][0] + x_sign * dx, tails_pos[i][1] + y_sign * dy)
 
                 ans.add(tails_pos[-1])
 
